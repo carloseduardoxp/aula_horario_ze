63c1c8404f2b2b3c43a96f5e45ae1024e3dc5366
Fernandofgs91 <162770609+Fernandofgs91@users.noreply.github.com>
2024-08-29 00:04:19 -0300

    Comentando APP

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
+    // Declaração da classe "App". Em Java, todos os programas precisam estar dentro de uma classe.
+
     public static void main(String[] args) throws Exception {
+        // O método "main" é o ponto de entrada para qualquer aplicação Java.
+  
         System.out.println("Hello, World!");
+        // "System.out.println" é um comando que imprime a mensagem passada como parâmetro no console.
+     
     }
 }
